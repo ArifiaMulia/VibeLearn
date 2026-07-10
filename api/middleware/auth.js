49d4bdb5e9069b5aa5c304efb051ddf473e10b23
ArifiaMulia <arifia.mulia@prasetia.co.id>
2026-07-10 15:16:27 +0700

    fix: change JWT verification failure status code from 403 to 401 to handle session expiry correctly

--- api/middleware/auth.js
+++ api/middleware/auth.js
@@ -7,7 +7,7 @@ const authenticateToken = (req, res, next) => {
   const token = authHeader && authHeader.split(' ')[1];
   if (!token) return res.sendStatus(401);
   jwt.verify(token, jwtSecret, (err, user) => {
-    if (err) return res.sendStatus(403);
+    if (err) return res.sendStatus(401);
     req.user = user;
     next();
   });
